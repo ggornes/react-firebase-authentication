8e005c3b5b52e1aac9a808c7f15b3c962a8b2563
ggornes <gerardogornes@gmail.com>
2019-10-18 12:28:03 +0800

    firebase config

--- src/components/Firebase/firebase_example.js
+++ src/components/Firebase/firebase_example.js
@@ -19,6 +19,22 @@ class Firebase {
 
         this.auth = app.auth();
     }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
+
+
 }
 
 export default Firebase;
